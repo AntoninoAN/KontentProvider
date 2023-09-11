1168695845908a2f6c387da2c4b31e9444a9f6c9
Antonino Ardines <antonino.ardines@mobileappscompany.us>
2023-09-11 12:32:09 -0400

    Create first your table schema, columns and table name

---
+++ app/src/main/java/com/example/model/common/DatabaseColumns.kt
@@ -0,0 +1,9 @@
+package com.example.model.common
+
+object ColumnSchema {
+    const val TABLE_NAME = "users"
+    const val COLUMN_FIRST_NAME = "first_name"
+    const val COLUMN_LAST_NAME = "last_name"
+    const val COLUMN_ADDRESS = "address"
+    const val COLUMN_PHONE_NUMBER = "phone_number"
+}
