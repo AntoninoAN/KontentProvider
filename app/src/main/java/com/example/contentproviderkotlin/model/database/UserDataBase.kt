0203667aee47dbbb4cbfd351008891bc29d889d2
Antonino Ardines <antonino.ardines@mobileappscompany.us>
2023-09-11 12:54:23 -0400

    Third step. Create the database

--- app/src/main/java/com/example/contentproviderkotlin/model/database/UserDataBase.kt
+++ app/src/main/java/com/example/contentproviderkotlin/model/database/UserDataBase.kt
@@ -3,6 +3,7 @@ package com.example.contentproviderkotlin.model.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.contentproviderkotlin.model.common.ColumnSchema
 
 class UserDataBaseLegacy(
     private val context: Context,
@@ -10,10 +11,18 @@ class UserDataBaseLegacy(
     private val databaseVersion: Int) : SQLiteOpenHelper(context, databaseName, null, databaseVersion) {
 
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("Not yet implemented")
+        db?.execSQL(
+            "CREATE TABLE ${ColumnSchema.TABLE_NAME} (" +
+            "${ColumnSchema.COLUMN_FIRST_NAME} varchar(255), " +
+            "${ColumnSchema.COLUMN_LAST_NAME} varchar(255), " +
+            "${ColumnSchema.COLUMN_ADDRESS} varchar(255), " +
+            "${ColumnSchema.COLUMN_PHONE_NUMBER} varchar(255))"
+        )
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        db?.execSQL(
+            "DROP TABLE ${ColumnSchema.TABLE_NAME}"
+        )
     }
 }
